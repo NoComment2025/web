237bdc00ffd7696737107f65963cd84cd4996715
이윤지 <yoonji0879@dgsw.hs.kr>
2025-09-16 20:39:50 +0900

    docs: variant값 주석 추가

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -10,21 +10,31 @@ const baseTextStyle =  css`
   text-align: ${(props) => props.textAlign || 'left'};
   `;
 
+/**
+ * @사용가능한_variant_값
+ *   - 'title': 제목용 텍스트 스타일 (큰 글자 크기와 굵기)
+ *   - 'caption': 작은 보조 텍스트 스타일
+ *   - 'success': 완료 메시지용 텍스트 스타일
+ *   - 'fail': 실패 메시지용 텍스트 스타일
+ * @예시 <Text variant="title">주제</Text>
+ * @예시 <Text variant="success">분석 완료</Text>
+ */
+
 const textVariants = {
   title: css`
-    font-size: 1.375rem;
+    font-size: 1.375rem; /* 약 22px */
     font-weight: 600;`,
 
   caption: css`
-    color: #7D7D7D;
+    color: #7D7D7D; /* 어두운 회색 */
   `,
 
   success: css`
-    color: #28B700;
+    color: #28B700; /* 밝은 녹색 */
   `,
 
   fail: css`
-    color: #B70000;
+    color: #B70000; /* 진한 빨강 */
   `
 }
 
@@ -33,10 +43,10 @@ const StyledText = styled.p`
   ${(props) => props.variant && textVariants[props.variant]}
 `
 
-
 function Text({children, ...props}) {
   return (
     <StyledText {...props}>{children}</StyledText>
   )
-
 }
+
+export default Text;
