be4070562a50b6c5d081e72afc9562be88b71550
이윤지 <yoonji0879@dgsw.hs.kr>
2025-09-16 23:48:20 +0900

    style: NavBar 스타일 및 styled-components로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Analysis from './pages/Analysis'
-import Main from './pages/main'
+import Index from './pages/index';
 import Mypage from './pages/mypage'
 import Record from './pages/record'
 import SignIn from './pages/signIn'
@@ -10,24 +10,28 @@ import SignUpID from './pages/signUp_ID'
 import SignUpPW from './pages/signUp_PW'
 import SignUpTAC from './pages/signUp_TAC'
 import Popup from './pages/popup'
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/analysis" element={<Analysis />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/record" element={<Record />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup-id" element={<SignUpID />} />
-          <Route path="/signup-pw" element={<SignUpPW />} />
-          <Route path="/signup-tac" element={<SignUpTAC />} />
-          <Route path="/popup" element={<Popup />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/analysis" element={<Analysis />} />
+            <Route path="/mypage" element={<Mypage />} />
+            <Route path="/record" element={<Record />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup-id" element={<SignUpID />} />
+            <Route path="/signup-pw" element={<SignUpPW />} />
+            <Route path="/signup-tac" element={<SignUpTAC />} />
+            <Route path="/popup" element={<Popup />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,123 @@
+import styled from "styled-components"
+import Text from "./Text"
+
+const NavBarContainer = styled.div`
+  background-color: #181818;
+  width: 283px;
+  height: 100vh;
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-family: pretendard;
+  font-size: 16px;
+`
+
+const Logo = styled.div`
+  width: 141px;
+  height: 33px;
+  margin: 50px 0 0 70px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`
+
+const Choice = styled.div`
+  margin: 0 0 200px 38px;
+`
+
+const ChoiceBtn = styled.button`
+  text-align: center;
+  width: 205px;
+  height: 60px;
+  color: #fff;
+  background: none;
+  border: none;
+  margin-bottom: 31px;
+  border-radius: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #172035;
+  }
+`
+
+const UserInf = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #fff;
+  background-color: #272727;
+  width: 283px;
+  height: 113px;
+  text-align: center;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+const User = styled.div`
+  display: flex;
+`
+
+const ProfilePicture = styled.div`
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  background-color: #7f7f7f;
+  margin: 26px 0 0 39px;
+`
+
+const LogOutBtn = styled.button`
+  background-color: #172035;
+  border: none;
+  color: #fff;
+  width: 196px;
+  height: 19px;
+  border-radius: 5px;
+  margin-top: 15px;
+  cursor: pointer;
+`
+
+function NavBar() {
+  const nickname = "변성우"
+  const userId = "b2ong222"
+
+  return (
+    <NavBarContainer>
+      <Logo>
+        <img src="../assets/ORATO-logo.png" alt="Orato logo" />
+      </Logo>
+
+      <Choice>
+        <ChoiceBtn>
+          <Text textAlign="center">발표 분석</Text>
+        </ChoiceBtn>
+        <ChoiceBtn>
+          <Text textAlign="center">기록</Text>
+        </ChoiceBtn>
+        <ChoiceBtn>
+          <Text textAlign="center">마이페이지</Text>
+        </ChoiceBtn>
+      </Choice>
+
+      <UserInf>
+        <User>
+          <ProfilePicture />
+          <div>
+            <Text $margin="26px 0 0 13px" fontWeight="500">{nickname} 님</Text>
+            <Text $margin="0px 0 0 13px" fontSize="11px" color="#cccccc">@{userId}</Text>
+          </div>
+        </User>
+        <div>
+          <LogOutBtn>
+            <Text textAlign="center" fontSize="11px">로그아웃</Text>
+          </LogOutBtn>
+        </div>
+      </UserInf>
+    </NavBarContainer>
+  )
+}
+
+export default NavBar
