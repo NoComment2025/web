b6ed11531b114c78c2cb5f9470f9c405fc690c4d
lee090626 <hyeongseogi46@gmail.com>
2025-09-17 17:08:41 +0900

    fix : 배치가 이상했던 버그 수정

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -1,4 +1,5 @@
-import "./";
+import "../styles/Analysis.css";
+import NavBar from '../components/NavBar';
 
 function Analysis() {
   function CategoryButton(props) {
@@ -6,7 +7,7 @@ function Analysis() {
       <button
         id={props.id}
         style={{
-          gridRow: '6/8',
+          gridRow: '7/8',
           backgroundColor: '#151F5A',
           border: 'none',
           borderRadius: '10px',
@@ -19,40 +20,43 @@ function Analysis() {
   }
 
   return (
-    <div className="analyze">
-      <form id="subject-form">
-        <div className="subject-box">
-            <span>주제</span>
-            <input
-            id="subject-input"
-            type="text"
-            placeholder="발표의 주제를 입력해주세요."
-            />
-            <CategoryButton id="category-balphyo" name="발표" />
-            <CategoryButton id="category-yeonseol" name="연설" />
-            <CategoryButton id="category-gangyeon" name="강연" />
-            <CategoryButton id="category-guitar" name="기타" />
+    <>
+      <div className="analyze">
+        <form id="subject-form">
+          <div className="subject-box">
+              <span>주제</span>
+              <input
+              id="subject-input"
+              type="text"
+              placeholder="발표의 주제를 입력해주세요."
+              />
+              <CategoryButton id="category-balphyo" name="발표" />
+              <CategoryButton id="category-yeonseol" name="연설" />
+              <CategoryButton id="category-gangyeon" name="강연" />
+              <CategoryButton id="category-guitar" name="기타" />
 
-            <div id='divid'></div>
+              <div id='divid'></div>
 
-            <label id="timeY">
-                <input type="radio" id="timelimitY" name="timelimit" value="있음" />
-                시간 제한 있음
-            </label>
+              <label id="timeY">
+                  <input type="radio" id="timelimitY" name="timelimit" value="있음" />
+                  시간 제한 있음
+              </label>
 
-            <label id='timeN'>
-                <input type="radio" id="timelimitN" name="timelimit" value="없음" />
-                시간 제한 없음
-            </label>
-            </div>
+              <label id='timeN'>
+                  <input type="radio" id="timelimitN" name="timelimit" value="없음" />
+                  시간 제한 없음
+              </label>
+              </div>
 
-        <div id="file-submit"></div>
+          <div id="file-submit"></div>
 
-        <button id="submit-button" type="submit">
-          다음
-        </button>
-      </form>
-    </div>
+          <button id="submit-button" type="submit">
+            다음
+          </button>
+        </form>
+      </div>
+      <NavBar></NavBar>
+    </>
   );
 }
 
