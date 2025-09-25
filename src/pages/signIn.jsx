a7dca39c9420b18b6e01f2bfb5e14602be6e19d8
세상을 이롭게하는 나 <b2ong091225@sesang-eul-ui-MacBookPro.local>
2025-09-25 23:47:03 +0900

    feat : 컴포넌트 넣음

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,29 +1,23 @@
 import logo from "../assets/ORATO-logo.png";
-import mike from "../assets/signin-mike-img.png"
-import '../styles/signIn.css'
+import mike from "../assets/signin-mike-img.png";
+import '../styles/signIn.css';
+import SignInUsername from "../components/signInUsername";
+import SignInPassword from "../components/signInPassword";
 import SignUp from "./signUp";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function SignIn() {
+  const [step, setStep] = useState(1);
   return (
     <div className="background">
       <div className="container">
         <div>
           <div className="leftContainer">
-            <div className="on">
-              <div>
-                <p style={{
-                  fontSize: '48px'
-                }}>발표</p>
-              </div>
-              <div>
-                <img className="mike" src={mike} alt="mike" />
-              </div>
+            <div className="on">  
+              <p style={{fontSize: '3em', display: 'flex',  alignItems: 'center'}}>발표<img className="mike" src={mike} alt="mike" /></p>
             </div>
-            
-            <p style={{
-              fontSize: '36px'
-            }}>이제 AI와 함께 완벽하게</p>
+            <p style={{fontSize: '36px'}}>이제 AI와 함께 완벽하게</p>
           </div>
         </div>
         <hr />
@@ -32,48 +26,29 @@ function SignIn() {
           <div>
             <img src={logo} alt="로고" className="logo" />
           </div>
-          
           <div>
-            <div className="inputContainer">
-              <label className="labelText">성명</label>
-              <input className="inputBox" type="text" placeholder="사용자 성명을 입력해주세요." />
-            </div>
-
-            <div className="inputContainer">
-              <label className="labelText">아이디</label>
-              <input className="inputBox" type="text" placeholder="아이디를 입력해주세요." />
-              <div className="pos">
-                <button className="isIdAvailable">중복 확인</button>
-              </div>
-            </div>
-
-            <div>
+            {step === 1? <SignInUsername setStep={setStep} /> : null}
+            {step === 2? <SignInPassword setStep={setStep} /> : null}
+          </div>
+          <div>
+            <div className="OAuthButton">
               <div>
-                <button className="nextButton">다음</button>
+                <img src="https://img.icons8.com/color/512/google-logo.png" alt="google"  className="imgSize" />
               </div>
 
-              <div className="OAuthButton">
-                <div>
-                  <img src="https://img.icons8.com/color/512/google-logo.png" alt="google"  className="imgSize" />
-                </div>
-
-                <div>
-                  <img src="https://wiki1.kr/images/thumb/c/cf/%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png/200px-%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png" alt="naver"  className="imgSize" />
-                </div>
-
-                <div>
-                  <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg" alt="kakao"  className="imgSize" />
-                </div>
+              <div>
+                <img src="https://wiki1.kr/images/thumb/c/cf/%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png/200px-%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png" alt="naver"  className="imgSize" />
               </div>
-              <div className="toLogInButtonContainer">
-                <Link to="/signup" className="toLogInButton">
-                  <SignUp />로그인
-                </Link>
+
+              <div>
+                <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg" alt="kakao"  className="imgSize" />
               </div>
-              
             </div>
-            
-            
+            <div className="toLogInButtonContainer">
+              <Link to="/signup" className="toLogInButton">
+                <SignUp />로그인
+              </Link>
+            </div>
           </div>
         </div>
       </div>
