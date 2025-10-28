d70d88546577a8274e588a7ec6fdde2734ed5997
세상을 이롭게하는 나 <b2ong091225@sesang-eul-ui-MacBookPro.local>
2025-10-28 21:34:02 +0900

    feat: 비밀번호 입력 컴포넌트

--- src/pages/signUp_PW.jsx
+++ src/pages/signUp_PW.jsx
@@ -1,6 +1,31 @@
-function SignUpPW() {
+import { useState } from "react";
+import eye from "../assets/eye.png";
+import noEye from "../assets/noEye.png";
+
+function SignUpPW({ password, setPassword, error }) {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div>
+    <div className="inputContainer">
+      <div className="labelRow">
+        <label className="labelText">비밀번호</label>
+        {error && <label className="warningText">{error}</label>}
+      </div>
+      <div className="inputWrapper">
+        <input
+          className="InputBox"
+          type={showPassword ? "text" : "password"}
+          placeholder="비밀번호를 입력해주세요."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <img
+          src={showPassword ? noEye : eye}
+          alt="비밀번호 보기"
+          onClick={() => setShowPassword(!showPassword)}
+          style={{ cursor: "pointer" }}
+        />
+      </div>
     </div>
   );
 }
