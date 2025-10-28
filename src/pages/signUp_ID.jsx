177bbaa573b7c07736ceec40e1079e91e90067ee
세상을 이롭게하는 나 <b2ong091225@sesang-eul-ui-MacBookPro.local>
2025-10-28 21:01:31 +0900

    feat: 아이디 입력 컴포넌트

--- src/pages/signUp_ID.jsx
+++ src/pages/signUp_ID.jsx
@@ -1,6 +1,17 @@
-function SignUpID() {
+function SignUpID({ id, setId, error }) {
   return (
-    <div>
+    <div className="inputContainer">
+      <div className="labelRow">
+        <label className="labelText">아이디</label>
+        {error && <label className="warningText">{error}</label>}
+      </div>
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="아이디를 입력해주세요."
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
     </div>
   );
 }
