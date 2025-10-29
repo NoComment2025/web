15d760b361b450d1e1d62cf2f23d81f6410deda6
bong <bongchok45@gmail.com>
2025-10-29 19:35:52 +0900

    fix: 로그인 ID

---
+++ src/pages/signIn_ID.jsx
@@ -0,0 +1,19 @@
+function SignInID({ id, setId, error }) {
+  return (
+    <div className="inputContainer">
+      <div className="labelRow">
+        <label className="labelText">아이디</label>
+        {error && <label className="warningText">{error}</label>}
+      </div>
+      <input
+        className="inputBox"
+        type="text"
+        placeholder="아이디를 입력해주세요."
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default SignInID;
