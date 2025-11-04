17d8061ebcecf0eb29ddd1e3b13ca1dc88845088
bong <bongchok45@gmail.com>
2025-11-04 23:47:01 +0900

    fix: 스타일드 컴포넌트로 변경

--- src/components/signUpPassword.jsx
+++ src/components/signUpPassword.jsx
@@ -1,9 +1,157 @@
-import '../styles/signUpPassword.css';
-import { Link } from "react-router-dom";
-import logo from "../assets/ORATO-logo.png";
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import eye from "../assets/eye.png";
 import noEye from "../assets/noEye.png";
-import { useState } from 'react';
+import Oauth from './oauth';
+
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const ContentBlock = styled.div`
+  margin-bottom: 3.3125rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const InputContainer = styled.div`
+  margin-bottom: 1.25em;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+
+const LabelRow = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 400px;
+    margin-bottom: 0.6rem;
+`;
+
+const LabelText = styled.label`
+    font-size: 1.4rem;
+    display: flex;
+`;
+
+const WarningText = styled.span`
+    align-content: center;
+    text-align: end;
+    font-size: 12px;
+    color: red;
+`;
+
+const InputWrapper = styled.div`
+    width: 100%;
+    max-width: 400px;
+    height: 50px;
+    background-color: #3b3b3b;
+    border: none;
+    border-radius: 15px;
+    padding: 0 15px; /* 좌우 패딩만 유지 */
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+`;
+
+const InputBox = styled.input`
+    background: none;
+    border: none;
+    flex: 1;
+    color: #fff;
+    font-size: 1rem;
+    height: 100%; 
+
+    &:focus {
+        outline: none;
+    }
+
+    &::placeholder {
+        color: #8C8C8C;
+    }
+`;
+
+const EyeIcon = styled.img`
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+    flex-shrink: 0;
+`;
+
+const ActionRow = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+    width: 100%;
+    max-width: 400px;
+`;
+
+const Button = styled.button`
+    border: none;
+    border-radius: 15px;
+    max-width: 190px;
+    width: 100%;
+    height: 52px;
+    color: #fff;
+    font-size: 1.375rem;
+    cursor: pointer;
+`;
+
+const BeforeButton = styled(Button)`
+    background-color: #8C8C8C;
+
+    &:active {
+        background-color: #7F7F7F;
+    }
+`;
+
+const SignUpButton = styled(Button)`
+    background-color: #3048D2;
+
+    &:active {
+        background-color: #1A1A93;
+    }
+`;
+
+const ToLogInButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`;
+
+const ToLogInLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: none;
+    max-width: 400px;
+    width: 100%;
+    height: 52px;
+    border: 1px #fff solid;
+    border-radius: 15px;
+    color: #fff;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    font-size: 1.375rem;
+    text-decoration: none;
+
+    &:hover {
+        background-color: #fff;
+        color: #000;
+    }
+`;
+
 
 function SignUpPassword({ setStep, password, setPassword, checkPassword, setCheckPassword }) {
   const [showPassword, setShowPassword] = useState(false);
@@ -31,78 +179,60 @@ function SignUpPassword({ setStep, password, setPassword, checkPassword, setChec
   };
 
   return (
-    <>
-      <div>
-        <div>
-          <img src={logo} alt="로고" className="logo" />
-        </div>
+    <FormWrapper>
+      <ContentBlock>
+        <InputContainer>
+          <LabelRow>
+            <LabelText>비밀번호</LabelText>
+            {errors.password && <WarningText>{errors.password}</WarningText>}
+          </LabelRow>
+          <InputWrapper>
+            <InputBox
+              type={showPassword ? "text" : "password"}
+              placeholder="비밀번호를 입력해주세요."
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+            <EyeIcon
+              src={showPassword ? noEye : eye}
+              alt="비밀번호 보기"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          </InputWrapper>
+        </InputContainer>
 
-        <div className='b'>
-          <div className="inputContainer">
-            <div className='labelRow'>
-              <label className="labelText">비밀번호</label>
-              {errors.password && <span className="warningText">{errors.password}</span>}
-            </div>
-            <div className="inputWrapper">
-              <input
-                className="InputBox"
-                type={showPassword ? "text" : "password"}
-                placeholder="비밀번호를 입력해주세요."
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-              <img
-                src={showPassword ? noEye : eye}
-                alt="비밀번호 보기"
-                onClick={() => setShowPassword(!showPassword)}
-              />
-            </div>
-          </div>
+        <InputContainer>
+          <LabelRow>
+            <LabelText>비밀번호 확인</LabelText>
+            {errors.checkPassword && <WarningText>{errors.checkPassword}</WarningText>}
+          </LabelRow>
+          <InputWrapper>
+            <InputBox
+              type={showPassword2 ? "text" : "password"}
+              placeholder="비밀번호를 다시 입력해주세요."
+              onChange={(e) => setCheckPassword(e.target.value)}
+              value={checkPassword}
+            />
+            <EyeIcon
+              src={showPassword2 ? noEye : eye}
+              alt="비밀번호 확인"
+              onClick={() => setShowPassword2(!showPassword2)}
+            />
+          </InputWrapper>
+        </InputContainer>
+      </ContentBlock>
 
-          <div className="inputContainer">
-            <div className='labelRow'>
-              <label className="labelText">비밀번호 확인</label>
-              {errors.checkPassword && <span className="warningText">{errors.checkPassword}</span>}
-            </div>
-            <div className="inputWrapper">
-              <input
-                className="InputBox"
-                type={showPassword2 ? "text" : "password"}
-                placeholder="비밀번호를 다시 입력해주세요."
-                onChange={(e) => setCheckPassword(e.target.value)}
-                value={checkPassword}
-              />
-              <img
-                src={showPassword2 ? noEye : eye}
-                alt="비밀번호 확인"
-                onClick={() => setShowPassword2(!showPassword2)}
-              />
-            </div>
-          </div>
-        </div>
+      <ActionRow>
+        <BeforeButton onClick={onClickBefore}>이전</BeforeButton>
+        <SignUpButton onClick={onClickNext}>회원가입</SignUpButton>
+      </ActionRow>
 
-        <div className='a'>
-          <button className="beforeButton" onClick={onClickBefore}>이전</button>
-          <button className="signInButton" onClick={onClickNext}>회원가입</button>
-        </div>
-      </div>
+      <Oauth />
 
-      <div className="OAuthButton">
-        <div>
-          <img src="https://img.icons8.com/color/512/google-logo.png" alt="google" className="imgSize" />
-        </div>
-        <div>
-          <img src="https://wiki1.kr/images/thumb/c/cf/%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png/200px-%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png" alt="naver" className="imgSize" />
-        </div>
-        <div>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg" alt="kakao" className="imgSize" />
-        </div>
-      </div>
-
-      <div className="toLogInButtonContainer">
-        <Link to="/signin" className="toLogInButton">로그인</Link>
-      </div>
-    </>
+      <ToLogInButtonContainer>
+        <ToLogInLink to="/signin">로그인</ToLogInLink>
+      </ToLogInButtonContainer>
+    </FormWrapper>
   );
 }
 
