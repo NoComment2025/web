fade030640ca645526c7c91e6ea4533fe2e6ab97
bong <bongchok45@gmail.com>
2025-11-04 23:49:05 +0900

    fix: 스타일드 컴포넌트로 변경

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,20 +1,172 @@
+import { useState, useEffect } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import { Link } from "react-router-dom";
+
 import mike from '../assets/mike-img.png';
 import logo from "../assets/ORATO-logo.png"
-import { Link } from "react-router-dom";
-import SignInID from './signIn_ID.jsx'
-import SignInPW from './signIn_PW.jsx'
-import { useState, useEffect } from 'react';
 import checked from '../assets/checked.png';
 import notCheck from '../assets/notCheck.png';
-import '../styles/signIn.css'
+import SignInID from './signIn_ID.jsx'
+import SignInPW from './signIn_PW.jsx'
+import OAuth from '../components/oauth.jsx'
+import signInBgImg from '../assets/signin-bg-img.png';
+
+
+const dummyUsers = [{ id: "test", password: "password" }]; 
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+`;
+
+const Background = styled.div`
+    background-image: url(${signInBgImg});
+    background-size: cover;
+    background-repeat: no-repeat;
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    max-width: 1100px;
+    height: 700px;
+    background-color: #181818;
+    border-radius: 25px;
+    color: #fff;
+`;
+
+const LeftContainer = styled.div`
+    width: 550px;
+    height: 700px;
+    text-align: center;
+    align-content: center;
+`;
+
+const RightContainer = styled.div`
+    width: 550px;
+    height: 700px;
+    text-align: center; 
+`;
+
+const OnDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const Separator = styled.hr`
+    border: 3px solid #272727;
+    height: auto;
+    margin: 2rem 0;
+    flex-shrink: 0;
+`;
+
+const TitleP = styled.p`
+    font-size: 3em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const MikeImage = styled.img.attrs({ src: mike, alt: "mike" })`
+    width: auto;
+    height: 1em;
+`;
+
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center; 
+  width: 100%;
+`;
+
+const LogoImage = styled.img.attrs({ src: logo, alt: "로고" })`
+    width: 202px;
+    margin: 3.5rem auto;
+    display: block;
+`;
+
+const IdCheckDiv = styled.div`
+    display: flex;
+    gap: 0.375rem;
+    cursor: pointer;
+    width: 100%;
+    max-width: 400px;
+    justify-content: flex-start; 
+    margin: 0.875rem 0 0.9375rem 0; 
+`;
+
+const AgreeAllImg = styled.img`
+    width: 20px;
+    height: 20px;
+`;
+
+const NextButton = styled.button`
+    background-color: #3048D2;
+    border: none;
+    border-radius: 15px;
+    max-width: 400px;
+    width: 100%;
+    height: 52px;
+    color: #fff;
+    font-size: 1.375rem;
+    cursor: pointer;
+
+    &:hover {
+        background-color: #1A1A93;
+    }
+`;
+
+const ToLogInButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    max-width: 400px;
+`;
+
+const ToLogInLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: none;
+    width: 100%; 
+    height: 52px;
+    border: 1px #fff solid;
+    border-radius: 15px;
+    color: #fff;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    font-size: 1.375rem;
+    text-decoration: none;
+
+    &:hover {
+        background-color: #fff;
+        color: #000;
+    }
+`;
+
+
+
+
 
 function SignIn() {
   const [id, setId] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({ id: "", password: "" });
   const [idChecked, setidChecked] = useState(false);
+  
 
-  // 페이지 로드 시 로컬스토리지에 저장된 아이디가 있으면 불러오기
   useEffect(() => {
     const savedId = localStorage.getItem("savedId");
     if (savedId) {
@@ -24,12 +176,6 @@ function SignIn() {
   }, []);
 
   const change = () => setidChecked((v) => !v);
-
-  // 더미 회원 데이터
-  const dummyUsers = [
-    { id: "testuser", password: "1234" },
-    { id: "alice", password: "abcd" }
-  ];
 
   const onClickLogin = () => {
     let tempErrors = { id: "", password: "" };
@@ -49,77 +195,74 @@ function SignIn() {
       return;
     }
 
-    // 아이디 존재 확인
     const user = dummyUsers.find(user => user.id === id);
     if (!user) {
       setErrors({ id: "존재하지 않는 아이디입니다.", password: "" });
       return;
     }
 
-    // 비밀번호 확인
     if (user.password !== password) {
       setErrors({ id: "", password: "비밀번호가 다릅니다." });
       return;
     }
 
-    // 로그인 성공
-    alert("로그인 성공!");
+
+    alert("로그인 성공!"); 
+    
     setErrors({ id: "", password: "" });
 
-    // idChecked가 true면 로컬스토리지에 아이디 저장
     if (idChecked) {
       localStorage.setItem("savedId", id);
-      console.log("아이디 저장됨:", id);
     } else {
       localStorage.removeItem("savedId");
     }
   };
 
   return (
-    <div className="background">
-      <div className="container">
-        <div className="leftContainer">
-          <div className="on">  
-            <p style={{fontSize: '3em', display: 'flex',  alignItems: 'center'}}>
-              발표<img className="mike" src={mike} alt="mike" />
-            </p>
-          </div>
-          <p style={{fontSize: '36px'}}>이제 AI와 함께 완벽하게</p>
-        </div>
+    <>
+      <GlobalStyle />
+      <Background>
+        <Container>
+          <LeftContainer>
+            <OnDiv>  
+              <TitleP>
+                발표<MikeImage />
+              </TitleP>
+            </OnDiv>
+            <p style={{fontSize: '36px'}}>이제 AI와 함께 완벽하게</p>
+          </LeftContainer>
 
-        <hr />
+          <Separator />
 
-        <div className="rightContainer">
-          <img src={logo} alt="로고" className="logo" />
+          <RightContainer>
+            <FormWrapper>
+                <LogoImage />
 
-          <SignInID id={id} setId={setId} error={errors.id} />
-          <SignInPW password={password} setPassword={setPassword} error={errors.password} />
-          
-          <div className="idCheck" onClick={change} >
-            <img 
-                src={idChecked ? checked : notCheck}
-                alt="아이디 저장"
-                className="agreeAllImg"
-            />
-            <p>아이디 저장</p>
-          </div>
+                <SignInID id={id} setId={setId} error={errors.id} />
+                <SignInPW password={password} setPassword={setPassword} error={errors.password} />
+                
+                <IdCheckDiv onClick={change} >
+                  <AgreeAllImg 
+                      src={idChecked ? checked : notCheck}
+                      alt="아이디 저장"
+                  />
+                  <p>아이디 저장</p>
+                </IdCheckDiv>
 
-          <button className="nextButton" onClick={onClickLogin}>
-            로그인
-          </button>
+                <NextButton onClick={onClickLogin}>
+                  로그인
+                </NextButton>
 
-          <div className="OAuthButton">
-            <img src="https://img.icons8.com/color/512/google-logo.png" alt="google" className="imgSize" />
-            <img src="https://wiki1.kr/images/thumb/c/cf/%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png/200px-%EB%84%A4%EC%9D%B4%EB%B2%84%E3%88%9C_%EB%A1%9C%EA%B3%A0.png" alt="naver" className="imgSize" />
-            <img src="https://upload.wikimedia.org/wikipedia/commons/e/e3/KakaoTalk_logo.svg" alt="kakao" className="imgSize" />
-          </div>
+                <OAuth />
 
-          <div className="toLogInButtonContainer">
-            <Link to="/signup" className="toLogInButton">회원가입</Link>
-          </div>
-        </div>
-      </div>
-    </div>
+                <ToLogInButtonContainer>
+                  <ToLogInLink to="/signup">회원가입</ToLogInLink>
+                </ToLogInButtonContainer>
+            </FormWrapper>
+          </RightContainer>
+        </Container>
+      </Background>
+    </>
   );
 }
 
